c1253490b9a787c4aec60b34396d991acc6e76ea
emeliehensfelt <emelie046@gmail.com>
2024-02-23 12:42:49 +0100

    skapat repo-klasserna

---
+++ Database/DbConnection/ResponseRepo.cs
@@ -0,0 +1,12 @@
+﻿namespace Database.DbConnection
+{
+    public class ResponseRepo
+    {
+        private readonly AppDbContext _context;
+
+        public ResponseRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ Database/DbConnection/TicketRepo.cs
@@ -0,0 +1,14 @@
+﻿namespace Database.DbConnection
+{
+    public class TicketRepo<T> where T : class
+    {
+        private readonly AppDbContext _context;
+
+        public TicketRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+    }
+
+}
