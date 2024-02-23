93355cc731bc8bc5f08546c41ff831184c7a19d7
JonhannesNilsson <jonte9203@hotmail.com>
2024-02-23 15:17:51 +0100

    new controllers

---
+++ App.Api/Controllers/ResponseController.cs
@@ -0,0 +1,57 @@
+﻿using Database.DbConnection;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Models;
+
+namespace App.Api.Controllers
+{
+    public class ResponseController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly GenericRepo<ResponseModel> _responseRepo;
+
+        public ResponseController(AppDbContext context, GenericRepo<ResponseModel> responseRepo)
+        {
+            _context = context;
+            _responseRepo = responseRepo;
+        }
+
+
+        [HttpGet("Responses")]
+        public ActionResult<List<TicketModel>> GetAllResponses()
+        {
+            var tickets = _responseRepo.GetAll();
+
+            return Ok(tickets);
+        }
+
+
+        [HttpPost("Responses")]
+        public ActionResult PostResponse(ResponseModel newTicket)
+        {
+            if (newTicket != null)
+            {
+                _responseRepo.Add(newTicket);
+
+                return Ok();
+            }
+
+            return BadRequest();
+        }
+
+
+        [HttpDelete("Response/{id}")]
+        public ActionResult DeleteResponse(int id)
+        {
+            var ticket = _context.Responses.FirstOrDefault(x => x.Id == id);
+
+            if (ticket != null)
+            {
+                _responseRepo.Delete(id);
+                return Ok(ticket);
+            }
+
+
+            return NoContent();
+        }
+    }
+}
