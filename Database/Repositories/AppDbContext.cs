d5a9ed5c928a2220ff64124bf7bf3ba8e16723c4
emeliehensfelt <emelie046@gmail.com>
2024-02-23 10:52:50 +0100

    mer dbcontext

--- Database/Repositories/AppDbContext.cs
+++ Database/Repositories/AppDbContext.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.Models;
 
 namespace Database.Repositories
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<TicketModel> Tickets { get; set; }
+        public DbSet<TicketTag> TicketTag { get; set; }
+        public DbSet<TagModel> Tags { get; set; }
+        public DbSet<ResponseModel> Responses { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Konfigurera många-till-många relationen mellan TicketModel och TagModel.
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<TicketTag>()
+                .HasKey(tt => new { tt.TicketId, tt.TagId });
+
+            // Konfigurera många-till-en relationen mellan TicketModel och TicketTagModel.
+            modelBuilder.Entity<TicketTag>()
+                .HasOne(tt => tt.Ticket)
+                .WithMany(t => t.TicketTags)
+                .HasForeignKey(tt => tt.TicketId);
+
+            // Konfigurera många-till-en relationen mellan TagModel och TicketTagModel.
+            modelBuilder.Entity<TicketTag>()
+                .HasOne(tt => tt.Tag)
+                .WithMany(t => t.TicketTags)
+                .HasForeignKey(tt => tt.TagId);
+
+            // Konfigurera en-till-många-relationen mellan TicketModel och ResponseModel.
+            modelBuilder.Entity<ResponseModel>()
+                .HasOne(r => r.Ticket)
+                .WithMany(t => t.Responses)
+                .HasForeignKey(r => r.TicketId);
+        }
     }
 }
+
