7421398da0c55ec622462adb464e4f110f28a5bc
EmmaDQ <sydemma@hotmail.com>
2024-02-23 15:22:24 +0100

    add viewservice

---
+++ AppLogic/Services/ViewServices/IViewServices.cs
@@ -0,0 +1,22 @@
+﻿using Shared.Models;
+using Shared.Models.Shared.Models;
+using static Shared.Models.ApiModels;
+
+namespace AppLogic.Services.ViewServices
+{
+    internal interface IViewServices
+    {
+        public TicketAPIModel TicketApi { get; set; }
+        public ResponseAPIModel ResponseApi { get; set; }
+        public TicketViewModel TicketView { get; set; }
+        public ResponseViewModel ResponseViewModel { get; set; }
+
+        public Task<List<TicketModel>> GetAPITickets();
+
+        public Task PostTicket(TicketAPIModel ticket);
+
+        public Task<List<TicketModel>> GetAPIResponse();
+
+        public Task PostTicket(ResponseAPIModel ticket);
+    }
+}
---
+++ AppLogic/Services/ViewServices/ViewServices.cs
@@ -0,0 +1,33 @@
+﻿using Shared.Models;
+using Shared.Models.Shared.Models;
+
+namespace AppLogic.Services.ViewServices
+{
+    internal class ViewServices : IViewServices
+    {
+        public ApiModels.TicketAPIModel TicketApi { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ApiModels.ResponseAPIModel ResponseApi { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public TicketViewModel TicketView { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public ResponseViewModel ResponseViewModel { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task<List<TicketViewModel>> GetAPIResponse()
+        {
+            foreach (TicketAPIModel)
+        }
+
+        public Task<List<TicketModel>> GetAPITickets()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostTicket(ApiModels.TicketAPIModel ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task PostTicket(ApiModels.ResponseAPIModel ticket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
